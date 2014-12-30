57e49f2556d69290d87b56070b61fece47bb5bb2
mdinger <mdinger.bugzilla@gmail.com>
2014-12-30 03:24:48 -0500

    Add Perl 6 default character classes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(macro_rules)]
 #![feature(slicing_syntax)]
+#![feature(default_type_params)]
 
 #[macro_export]
 macro_rules! matches(
@@ -14,8 +15,9 @@ macro_rules! matches(
     );
 );
 
+#[deriving(Show)]
 pub struct Rule {
-    regex: String,
+    regex: Vec<CharClass>,
 }
 
 enum Mode {
@@ -23,9 +25,118 @@ enum Mode {
     Scan,
 }
 
+#[deriving(Show)]
+enum CharClass {
+    Char(char),
+    Digit(bool),
+    Dot,
+    Newline(bool),
+    Tab(bool),
+    Whitespace(bool),
+    HWhitespace(bool),
+    Word(bool),
+}
+
+impl PartialEq<char> for CharClass {
+    fn eq(&self, c: &char) -> bool {
+        match *self {
+            CharClass::Char(s) => s == *c,
+            CharClass::Digit(digit) => {
+                let res = matches!(*c, '0' ... '9');
+                if digit { res } else { !res }
+            },
+            CharClass::Dot => { true },
+            CharClass::Tab(tab) => {
+                let res = matches!(*c, '\t');
+                if tab { res } else { !res }
+            },
+            CharClass::Whitespace(white) => {
+                let res = matches!(*c, ' ' | '\t' | '\n');
+                if white { res } else { !res }
+            },
+            CharClass::HWhitespace(h_white) => {
+                let res = matches!(*c, ' ' | '\t');
+                if h_white { res } else { !res }
+            },
+            CharClass::Newline(is) => {
+                let res = matches!(*c, '\n');
+                if is { res } else { !res }
+            },
+            CharClass::Word(word) => {
+                let res = matches!(*c, 'a' ... 'z' |
+                                       'A' ... 'Z' |
+                                       '0' ... '9' |
+                                       '_' );
+                if word { res } else { !res }
+            },
+        }
+    }
+}
+
 impl Rule {
-    pub fn new(s: &str) -> Result<Rule, ()> {
-        Ok(Rule { regex: s.to_string() })
+    pub fn new(s: &str) -> Result<Rule, &'static str> {
+        let mut chars = s.chars();
+        let mut regex: Vec<CharClass> = vec![];
+        
+        while let Some(c) = chars.next() {
+            match c {
+                'a' ... 'z' | 'A' ... 'Z' | '0' ... '9' | '_'
+                     => regex.push(CharClass::Char(c)),
+                '.'  => regex.push(CharClass::Dot),
+                ' ' | '\t' | '\n' => { continue; },
+                '\\' => {
+                    let char_class = match chars.next() {
+                        Some('d') => CharClass::Digit(true),
+                        Some('D') => CharClass::Digit(false),
+                        Some('h') => CharClass::HWhitespace(true),
+                        Some('H') => CharClass::HWhitespace(false),
+                        Some('n') => CharClass::Newline(true),
+                        Some('N') => CharClass::Newline(false),
+                        Some('s') => CharClass::Whitespace(true),
+                        Some('S') => CharClass::Whitespace(false),
+                        Some('t') => CharClass::Tab(true),
+                        Some('T') => CharClass::Tab(false),
+                        Some('w') => CharClass::Word(true),
+                        Some('W') => CharClass::Word(false),
+                        Some(c)   => CharClass::Char(c),
+                        None => { return Err(r"Last character (`\`) is invalid") },
+                    };
+                    
+                    regex.push(char_class);
+                },
+                '\'' => {
+                	let mut closed = false;
+                    while let Some(c) = chars.next() {
+                        match c {
+                            '\'' => {
+                            	closed = true;
+                            	break;
+                        	}
+                            c => regex.push(CharClass::Char(c)),
+                        }
+                    }
+
+					if !closed { return Err(r"The sets of `'` are unbalanced.") }
+                },
+                '"' => {
+                	let mut closed = false;
+                    while let Some(c) = chars.next() {
+                        match c {
+                            '"' => {
+                            	closed = true;
+                            	break;
+                        	}
+                            c => regex.push(CharClass::Char(c)),
+                        }
+                    }
+
+                    if !closed { return Err(r#"The sets of `"` are unbalanced."#) }
+                },
+                _ => { continue; },
+            }
+        }
+        
+        Ok(Rule { regex: regex })
     }
     
     pub fn is_match(self, s: &str) -> bool {
@@ -35,13 +146,21 @@ impl Rule {
         loop {
             match mode {
                 Mode::Match => {
-                    let mut count = 0u;
-                    while let Some(c) = chars.next() {
-                        count += 1;
-                        if count == self.regex.len() { break; }
-                        else if c != self.regex[].char_at(count) {
-                            mode = Mode::Scan;
-                            break;
+                    let mut first = true;
+                    for regex in self.regex.iter() {
+                        // Scan already checked first character so skip iteration.
+                        if first {
+                            first = false;
+                            continue;
+                        }
+                        
+                        if let Some(c) = chars.next() {
+                            println!("c: {}, regex: {}", c, regex);
+                            // Issue 19035: swapping cmp order breaks code
+                            if *regex != c  {
+                                mode = Mode::Scan;
+                                break;
+                            }
                         }
                     }
 
@@ -51,14 +170,14 @@ impl Rule {
                 },
                 Mode::Scan => {
                     while let Some(c) = chars.next() {
-                        if c == self.regex[].char_at(0) {
+                        // Issue 19035: swapping cmp order breaks code
+                        if self.regex[0] == c {
                             mode = Mode::Match;
                             break;
                         }
                     }
                     
-                    // If finished scanning string but still needs scanning,
-                    // then there isn't a match in the str.
+                    // If finished scanning string but no match, there isn't a match.
                     if let Mode::Scan = mode { return false; }
                 },
             };
--- tests/lib.rs
+++ tests/lib.rs
@@ -26,11 +26,73 @@ fn matches_substr() {
     match_succeed("fred", "this_fred");
     match_succeed("fred", "fred_that");
     match_succeed("fred", "this_fred_and_that");
-    match_succeed("fred", "this_fiend_and_fred_and_that");
+    // chars match correctly inside `'` sets.
+    match_succeed("f're''d'", "this_fred_and_that");
   
     match_fail("fred", "coconut");
     match_fail("fred", "friend");
     match_fail("fred", "this_fiend_and");
 }
 
+#[test]
+fn dot_matches() {
+    match_succeed(r"d.t", r"dot");
+    match_succeed(r"d..", r"d  \n");
+    match_succeed(r"d..", r"d\t\t");
+    // d + 2 newlines
+    match_succeed(r"d..", r"d
 
+");
+}
+
+#[test]
+fn digits_match() {
+    match_succeed(r"\d . apples", r"7 apples");
+    match_succeed(r"\d\d / \d\d / \d\d\d\d", r"07/14/1963");
+    // 4 non-digits
+    match_succeed(r"\D\D\D\D", r"fred");
+}
+
+#[test]
+fn horizontal_whitespace_matches() {
+	// 2 spaces
+    match_succeed(r"\h\h", r"  ");
+    // 3 colons separated by tabs
+    match_succeed(r"\: \h \: \h \:", r":	:	:");
+    // 3 colons separated by actual tabs
+    match_succeed(r"':	:	:'", r":	:	:");
+    // paper cut surrounded by 3 non-whitespace characters
+    match_succeed(r"\H paper \H cut \H", r":paper:cut:");
+}
+
+#[test]
+fn vertical_whitespace_matches() {
+    match_succeed(r"use \n newline \n character", r"use
+newline
+character");
+}
+
+#[test]
+fn whitespace_matches() {
+    match_succeed(r"match \t tabs \n newlines", r"match	tabs
+newlines");
+	// Match 3 non-tab, non-newline
+    match_succeed(r"\T\N\N", r"are");
+	// Same as previous but match actual characters (not escaped sequences)
+    match_succeed(r"'match	tabs
+newlines'", r"match	tabs
+newlines");
+	// `\s` matches all whitespace
+    match_succeed(r"match \s tabs \s newlines", r"match	tabs
+newlines");
+	// Match 3 non whitespace
+    match_succeed(r"\S\S\S", r"are");
+}
+
+#[test]
+fn words_match() {
+	// `\w` matches digits, characters, underscores.
+    match_succeed(r"\w\w\w \w \w", r"L8t_e");
+    // Match 3 non-word characters
+    match_succeed(r"\W \W \W", r"&*$");
+}
