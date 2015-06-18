3b648c91c43aa787003a7d11422cdcec92be0e0d
mdinger <mdinger.bugzilla@gmail.com>
2015-06-17 20:17:09 -0400

    Add set union test

--- src/range_set.rs
+++ src/range_set.rs
@@ -113,4 +113,14 @@ mod test {
     fn panic_on_decreasing_order() {
         generate(vec![('5', '3')]);
     }
+    #[test]
+    fn set_union() {
+        let mut set1 = generate(vec![('1', '4'), ('5', '6')]);
+        let set2 =     generate(vec![('0', '5'), ('8', '9')]);
+
+        set1.union(set2);
+
+        let other = generate(vec![('0', '6'), ('8', '9')]);
+        assert_eq!(set1, other);
+    }
 }
