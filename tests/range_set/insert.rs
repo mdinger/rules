5035eae762a5eb9b2e6f9d9e65f686bc05121502
mdinger <mdinger.bugzilla@gmail.com>
2015-07-05 01:15:29 -0400

    Clean up insert tests

--- tests/range_set/insert.rs
+++ tests/range_set/insert.rs
@@ -2,33 +2,41 @@ use super::generate;
 
 #[test]
 fn partial_overlap() {
-    let set1 = generate(vec![('3', '5'), ('4', '6')]);
-    let set2 = generate(vec![('3', '5'), ('1', '4')]);
+    let left  = generate(vec![('3', '5'), ('4', '6')]);
+    let right = generate(vec![('3', '5'), ('1', '4')]);
 
-    let other1 = generate(vec![('3', '6')]);
-    let other2 = generate(vec![('1', '5')]);
+    let other_left  = generate(vec![('3', '6')]);
+    let other_right = generate(vec![('1', '5')]);
 
-    assert_eq!(other1, set1);
-    assert_eq!(other2, set2);
+    assert_eq!(left, other_left);
+    assert_eq!(right, other_right);
 }
 #[test]
 fn subset() {
-    let set1 = generate(vec![('1', '5'), ('2', '3')]); // Complete overlap.
-    let set2 = generate(vec![('1', '5'), ('1', '3')]); // Left is exact.
-    let set3 = generate(vec![('1', '5'), ('2', '5')]); // Right is exact.
-    let set4 = generate(vec![('1', '5'), ('1', '5')]); // Both are exact.
+    let inner = generate(vec![('1', '5'), ('2', '3')]);
+    let left  = generate(vec![('1', '5'), ('1', '3')]);
+    let right = generate(vec![('1', '5'), ('2', '5')]);
+    let both  = generate(vec![('1', '5'), ('1', '5')]);
 
     let other = generate(vec![('1', '5')]);
 
-    assert_eq!(set1, other);
-    assert_eq!(set2, other);
-    assert_eq!(set3, other);
-    assert_eq!(set4, other);
+    assert_eq!(inner, other);
+    assert_eq!(left, other);
+    assert_eq!(right, other);
+    assert_eq!(both, other);
 }
 #[test]
 fn superset() {
     let set   = generate(vec![('3', '5'), ('2', '6')]);
     let other = generate(vec![('2', '6')]);
+
+    assert_eq!(other, set);
+}
+#[test]
+// Useless test but added for completeness.
+fn disjoint() {
+    let set   = generate(vec![('2', '4'), ('6', '8')]);
+    let other = generate(vec![('2', '4'), ('6', '8')]);
 
     assert_eq!(other, set);
 }
