403c51bbcd4b3dd7fd19ce609add1109c2ed8fab
mdinger <mdinger.bugzilla@gmail.com>
2015-07-12 15:02:57 -0400

    Add tests to range_set union

--- tests/range_set/union.rs
+++ tests/range_set/union.rs
@@ -1,12 +1,60 @@
 use super::generate;
 
 #[test]
-fn test() {
-    let set1 = generate(vec![('1', '4'), ('5', '6')]);
-    let set2 = generate(vec![('0', '5'), ('8', '9')]);
+fn partial_overlap() {
+    let left  = generate(vec![('3', '4')]);
+    let right = generate(vec![('4', '6')]);
 
-    let union = set1.union(&set2);
+    let other = generate(vec![('3', '6')]);
 
-    let other = generate(vec![('0', '6'), ('8', '9')]);
-    assert_eq!(union, other);
+    assert_eq!(left.union(&right), other);
+    assert_eq!(right.union(&left), other);
+}
+
+#[test]
+fn subset() {
+    let set = generate(vec![('1', '5')]);
+
+    let inner = generate(vec![('2', '3')]);
+    let left  = generate(vec![('1', '3')]);
+    let right = generate(vec![('2', '5')]);
+    let both  = generate(vec![('1', '5')]);
+
+    let other = generate(vec![('1', '5')]);
+
+    assert_eq!(set.union(&inner), other);
+    assert_eq!(set.union(&left), other);
+    assert_eq!(set.union(&right), other);
+    assert_eq!(set.union(&both), other);
+}
+
+#[test]
+fn superset() {
+    let set      = generate(vec![('3', '5')]);
+    let superset = generate(vec![('2', '6')]);
+
+    assert_eq!(set.union(&superset), superset);
+    assert_eq!(superset.union(&set), superset);
+}
+
+#[test]
+fn disjoint() {
+    let left  = generate(vec![('2', '4')]);
+    let right = generate(vec![('6', '8')]);
+
+    let other = generate(vec![('2', '4'), ('6', '8')]);
+
+    assert_eq!(left.union(&right), other);
+    assert_eq!(right.union(&left), other);
+}
+
+#[test]
+fn disjoint_extend() {
+    let left  = generate(vec![('2', '4')]);
+    let right = generate(vec![('5', '8')]);
+
+    let other = generate(vec![('2', '8')]);
+
+    assert_eq!(left.union(&right), other);
+    assert_eq!(right.union(&left), other);
 }
