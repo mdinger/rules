cc1d4fc980e91dbbafbd15cfc13b2d8381996d0d
mdinger <mdinger.bugzilla@gmail.com>
2015-07-26 23:48:24 -0400

    Get `is_match()` working with literals. Add some tests.

--- src/re.rs
+++ src/re.rs
@@ -2,30 +2,63 @@ use parse::{self, Ast};
 use collapse;
 
 #[derive(Debug)]
-pub struct Regex {
+pub struct Regex(Vec<Ast>);
+
+impl Regex {
+    pub fn new(s: &str) -> Regex {
+        let vec = parse::parse(s).unwrap();
+        let vec = collapse::collapse(vec);
+
+        Regex(vec)
+    }
+    pub fn is_match(&self, s: &str) -> bool {
+        let mut pair = Pair { ast: self.0.clone(), pos_ast: 0,
+                              chars: s.chars().collect(), pos_char: 0 };
+        let mut count = 0;
+
+        loop {
+            if pair.matches_ast() {
+                if !pair.next_ast() { return true }
+            } else { pair.pos_ast = 0 }
+
+            if !pair.next_char() { break }
+            count += 1;
+            if count > 20 { break }
+        }
+
+        false
+    }
+}
+
+// A structure to allow a Regex to determine if it matches the &str.
+#[derive(Debug)]
+struct Pair {
     ast: Vec<Ast>,
-    chars: Vec<char>,
     pos_ast: usize,
+    chars: Vec<char>,
     pos_char: usize,
 }
 
-impl Regex {
-    fn ast_contains(&self) -> bool {
+impl Pair {
+    fn cur_ast(&self) -> Ast { self.ast[self.pos_ast].clone() }
+    fn cur_char(&self) -> char { self.chars[self.pos_char] }
+    fn matches_ast(&mut self) -> bool {
         let cur = self.cur_char();
 
         match self.cur_ast() {
             Ast::Char(c) => c == cur,
-            Ast::Literal(_) => unimplemented!(),
+            Ast::Literal(ref lit) => {
+                let str_lit: String = lit.iter().map(|&x| x).collect();
+                let str_chars: String = self.chars[self.pos_char..].iter().map(|&x| x).collect();
+
+                if let Some(start_pos) = str_chars.find(&str_lit) {
+                    self.pos_char += start_pos + str_lit.len() - 1;
+
+                    true
+                } else { false }
+            },
             _ => unimplemented!(),
         }
-    }
-    fn cur_ast(&self) -> Ast { self.ast[self.pos_ast].clone() }
-    fn cur_char(&self) -> char { self.chars[self.pos_char] }
-    pub fn new(s: &str) -> Regex {
-        let vec = parse::parse(s).unwrap();
-        let vec = collapse::collapse(vec);
-        
-        Regex { ast: vec, chars: vec![], pos_ast: 0, pos_char: 0 }
     }
     fn next_ast(&mut self) -> bool {
         self.pos_ast += 1;
@@ -36,18 +69,5 @@ impl Regex {
         self.pos_char += 1;
 
         self.pos_char != self.chars.len()
-    }
-    pub fn is_match(&mut self, s: &str) -> bool {
-        self.chars = s.chars().collect();
-
-        loop {
-            if self.ast_contains() {
-                if !self.next_ast() { return true }
-            } else { self.pos_ast = 0 }
-
-            if !self.next_char() { break }
-        }
-
-        false
     }
 }
--- tests/re/is_match.rs
+++ tests/re/is_match.rs
@@ -2,21 +2,26 @@ use rules::re::Regex;
 
 #[test]
 fn chars() {
-    let mut re = Regex::new(r"Apple");
+    let re = Regex::new(r"Apple");
     assert!(re.is_match("I have an Apple. Do you?"));
 }
 #[test]
 fn whitespace_escapes() {
-    let mut re = Regex::new(r"ping\ pong");
+    let re = Regex::new(r"ping\ pong");
     assert!(re.is_match("Do you like ping pong? I do."));
 }
-/*#[test]
+#[test]
 fn literals_ticks() {
-    let mut re = Regex::new(r"'two apples'");
+    let re = Regex::new(r"'two apples'");
     assert!(re.is_match("Are there two apples? Really?"));
 }
 #[test]
 fn literals_quotes() {
-    let mut re = Regex::new(r#""two apples""#);
+    let re = Regex::new(r#""two apples""#);
     assert!(re.is_match("Are there two apples? Really?"));
-}*/
+}
+#[test]
+fn literals_regular() {
+    let re = Regex::new(r#"'2' and "3" and "4" and '5'"#);
+    assert!(re.is_match("Well, 2and3and4and5 there."));
+}
