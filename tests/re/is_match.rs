ea14585d1e37640b972731de3eb30aa9047a5230
mdinger <mdinger.bugzilla@gmail.com>
2015-08-08 18:23:34 -0400

    Update whitespace in tests

--- tests/re/is_match.rs
+++ tests/re/is_match.rs
@@ -14,25 +14,25 @@ fn whitespace_escapes() {
 }
 #[test]
 fn literals_ticks() {
-    let re = Regex::new(r"'two apples'");
+    let re = Regex::new(r" 'two apples' ");
     assert!( re.is_match("Are there two apples? Really?"));
     assert!(!re.is_match("Are there two grapes? Really?"));
 }
 #[test]
 fn literals_quotes() {
-    let re = Regex::new(r#""two apples""#);
+    let re = Regex::new(r#" "two apples" "#);
     assert!( re.is_match("Are there two apples? Really?"));
     assert!(!re.is_match("Are there two grapes? Really?"));
 }
 #[test]
 fn literals_regular() {
-    let re = Regex::new(r#"'2' and "3" and "4" and '5'"#);
+    let re = Regex::new(r#" '2' and "3" and "4" and '5' "#);
     assert!( re.is_match("Well, 2and3and4and5 there."));
     assert!(!re.is_match("Well, 5and4and3and2 there."));
 }
 #[test]
 fn char_class_range() {
-    let re = Regex::new(r"<[a .. z]>");
+    let re = Regex::new(r"<[ a .. z ]>");
     assert!( re.is_match("a"));
     assert!( re.is_match("p"));
     assert!( re.is_match("z"));
@@ -40,7 +40,6 @@ fn char_class_range() {
     assert!(!re.is_match("9"));
     assert!(!re.is_match("こ"));
 }
-
 #[test]
 fn char_class_multi() {
     let re = Regex::new(r"<[ 0 .. 9 ]> <[ a .. z ]>");
